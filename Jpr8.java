599a189bacbd20203e71b57ea21767f0901ccfab
ManaliBabar <manalibabar25@gmail.com>
2021-08-13 12:45:41 +0530

    Assign 7,8,9,10

---
+++ Jpr8.java
@@ -0,0 +1,40 @@
+package com.Training;
+
+public class Jpr7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 /* Arithmatic operators */
+		int result=1+2;
+		System.out.println("1+2= "+result);
+		
+		int Original_result=result;
+		result=result-1;
+		// result value is 2
+		
+		System.out.println(Original_result +"-1="+result);
+		
+		Original_result=result;
+		result=result*2;
+		// result value is 4
+		System.out.println(Original_result +"*2="+result);
+		
+		Original_result=result;
+		result=result/2;
+		// result value is 2
+		System.out.println(Original_result +"/2="+result);
+		
+		Original_result=result;
+		result=result+8;
+		// result value is 10
+		System.out.println(Original_result +"+8="+result);
+		
+		Original_result=result;
+		result=result%7;
+		// result value is 3
+		System.out.println(Original_result +"%7="+result);
+
+
+	}
+
+}
