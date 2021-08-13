ffdf553bb841e3bc69180230c65ff467770e281e
Manali_Mrunali_Babar <88653010+MrunaliBabar@users.noreply.github.com>
2021-08-13 12:48:19 +0530

    Assign 8

--- Jpr8.java
+++ Jpr8.java
@@ -5,34 +5,19 @@ public class Jpr7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 /* Arithmatic operators */
-		int result=1+2;
-		System.out.println("1+2= "+result);
-		
-		int Original_result=result;
-		result=result-1;
-		// result value is 2
-		
-		System.out.println(Original_result +"-1="+result);
-		
-		Original_result=result;
-		result=result*2;
-		// result value is 4
-		System.out.println(Original_result +"*2="+result);
-		
-		Original_result=result;
-		result=result/2;
-		// result value is 2
-		System.out.println(Original_result +"/2="+result);
-		
-		Original_result=result;
-		result=result+8;
-		// result value is 10
-		System.out.println(Original_result +"+8="+result);
-		
-		Original_result=result;
-		result=result%7;
-		// result value is 3
-		System.out.println(Original_result +"%7="+result);
+		int result=1;
+		result+=2;
+		System.out.println(result);// result is 3
+		result--;
+		System.out.println(result);// result value is 2
+		result*=2;
+		System.out.println(result);// result value is 4
+		result/=2;
+		System.out.println(result);// result value is 2
+		result+=8;
+		System.out.println(result);// result value is 10
+		result%=7;
+		System.out.println(result);// result value is 3
 
 
 	}
